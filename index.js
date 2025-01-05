fcdccbdf368288d01d3680099e930b7d0f52eae9
SAI ABHISHEK KETEPALLY <ketepask@mail.uc.edu>
2025-01-05 05:06:54 -0300

    added loggers

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url'; // Required for ES Modules to get the current directory
+import logger from "./logger.js";
+import morgan from "morgan";
+
+const morganFormat = ":method :url :status :response-time ms";
 
 const app = express();
 const port = 3000;
+
+app.use(
+    morgan(morganFormat, {
+      stream: {
+        write: (message) => {
+          const logObject = {
+            method: message.split(" ")[0],
+            url: message.split(" ")[1],
+            status: message.split(" ")[2],
+            responseTime: message.split(" ")[3],
+          };
+          logger.info(JSON.stringify(logObject));
+        },
+      },
+    })
+  );
 
 // Fix __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -22,6 +42,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/teas', (req, res) => { // add new tea
+    logger.warn("A post request was made to add new tea");
     const {name, price} = req.body // destructuring on the go! classic js 101
     const newTea = {
         id: nextId++,
